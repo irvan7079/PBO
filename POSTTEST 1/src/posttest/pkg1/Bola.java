05cc4d024600511f36e552e3764e97e5a024d566
Muhammad Irvan Hakim <93089454+irvan7079@users.noreply.github.com>
2023-03-07 23:51:55 +0800

    Add files via upload

---
+++ POSTTEST 1/src/posttest/pkg1/Bola.java
@@ -0,0 +1,36 @@
+package posttest.pkg1;
+
+public class Bola {
+    public String merk, jenis;
+    int stok, harga;
+
+    public Bola (String a, String b, int c, int d){
+        this.merk = a;
+        this.jenis = b;
+        this.stok = c;
+        this.harga = d;
+    }
+
+    public void display() {
+        System.out.println("Merk    : " + this.merk);
+        System.out.println("Jenis   : " + this.jenis);
+        System.out.println("stok    : " + this.stok);
+        System.out.println("harga   : " + this.harga);
+    }
+
+    public void setMerk(String merk_bola) {
+        this.merk = merk_bola;
+    }
+    
+    public void setJenis(String jenis_bola) {
+        this.jenis = jenis_bola;
+    }
+
+    public void setStok(int stok_bola) {
+        this.stok = stok_bola;
+    }
+    
+    public void setHarga(int Harga_bola) {
+        this.harga = Harga_bola;
+    }
+}
---
+++ POSTTEST 1/src/posttest/pkg1/POSTTEST1.java
@@ -0,0 +1,112 @@
+package posttest.pkg1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.IOException;
+/**
+ *
+ * @author 2109106057_Muhammad Irvan Hakim
+ */
+public class POSTTEST1 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        String MerkBola, JenisBola;
+        int StokBola, HargaBola;
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        ArrayList<Bola> dt = new ArrayList<>();
+        while (true) {
+            System.out.println("""
+                            \n
+                            ====================================
+                                PENDATAAN BOLA INAZUMA SPORT         
+                            ====================================
+                            | 1. Tambah Bola                   |
+                            | 2. Lihat Bola                    |
+                            | 3. Edit Bola                     |
+                            | 4. Hapus Bola                    |
+                            | 5. Keluar Program                |
+                            ====================================
+                            """);
+            System.out.print("Masukkan Pilihan Anda: ");
+            int select = Integer.parseInt(br.readLine());
+            switch (select) {
+                case 1 -> {
+                    System.out.println("=========== Tambah Bola ============");
+                    System.out.print("Merk Bola : ");
+                    MerkBola = br.readLine(); 
+                    System.out.print("Jenis Bola : ");
+                    JenisBola = br.readLine();
+                    System.out.print("Jumlah Stok Bola : ");
+                    StokBola = Integer.parseInt(br.readLine());
+                    System.out.print("Harga Bola : ");
+                    HargaBola = Integer.parseInt(br.readLine());
+
+                    Bola dataBola = new Bola(MerkBola, JenisBola, StokBola, HargaBola);
+                    dt.add(dataBola);
+                    System.out.println("====================================");
+                    System.out.println("       Bola Berhasil Ditambah!      ");
+                    System.out.println("====================================");
+                }
+
+                case 2 -> {
+                    System.out.println("============ Lihat Bola ============");
+                    for (int i = 0; i < dt.size(); i += 1) {
+                        System.out.println("Data Bola ke-" + (i + 1));
+                        dt.get(i).display();
+                    }
+                    System.out.println("====================================");
+                    System.out.println("      Bola Berhasil Ditampilkan!    ");
+                    System.out.println("====================================");
+                }
+                    
+                case 3 -> {
+                    System.out.println("============ Edit Bola =============");
+                    System.out.print("Input Merk Bola Yang Ingin Di edit : ");
+                    String EditMerk = br.readLine();
+                    for (Bola bola : dt) { 
+                        if (bola.merk.equals(EditMerk)) {
+                            System.out.print("Input Merk : ");
+                            bola.setMerk(br.readLine());
+                            System.out.print("Input Jenis : ");
+                            bola.setJenis(br.readLine());
+                            System.out.print("Input Stok Bola : ");
+                            bola.setStok(Integer.parseInt(br.readLine()));
+                            System.out.print("Input Harga Bola : ");
+                            bola.setHarga(Integer.parseInt(br.readLine()));
+                            System.out.println("====================================");
+                            System.out.println("        Bola Berhasil Diedit!       ");
+                            System.out.println("====================================");
+                            break;
+                        }
+                    }
+                }
+                    
+                case 4 -> {
+                    System.out.println("============ Hapus Bola ============");
+                    System.out.print("Input Merk Bola Yang Ingin Di hapus : ");
+                    String HapusMerk = br.readLine();
+                    for (int i = 0; i < dt.size(); i++) {
+                        if (dt.get(i).merk.equals(HapusMerk)) {
+                            dt.remove(i);
+                            System.out.println("====================================");
+                            System.out.println("        Bola Berhasil Dihapus!      ");
+                            System.out.println("====================================");
+                            break;
+                        }
+                    }
+                }
+                    
+                case 5 -> {
+                    System.out.println("""
+                                                           ====================================
+                                                               Berhasil keluar dari program!
+                                                           ====================================
+                                                           """);
+                    System.exit(0);
+                }
+                default -> System.out.println("Menu tidak ada");
+            }
+        }
+    }
+}
